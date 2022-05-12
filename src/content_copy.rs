54d5065ff0e0184ed11effb5d4cecaf231952bfd
trasua <hyouka266@gmail.com>
2022-05-12 20:41:08 +0700

    migrate from rust-clipboard -> copypasta

--- src/content_copy.rs
+++ src/content_copy.rs
@@ -1,16 +1,15 @@
-// use clipboard::{ClipboardContext, ClipboardProvider};
-use clipboard_ext::prelude::*;
-use clipboard_ext::x11_bin::ClipboardContext;
+use copypasta_ext::prelude::*;
+use copypasta_ext::x11_bin::ClipboardContext;
 use std::error::Error;
 use std::fs::File;
 use std::io::Read;
 
 pub fn clipboard_copy(src_file_path: &str) -> Result<(), Box<dyn Error>> {
     let mut src_file = File::open(src_file_path)?;
-    let mut ctx: ClipboardContext = ClipboardProvider::new()?;
+    let mut ctx = ClipboardContext::new().unwrap();
     let mut buffer: String = String::new();
     src_file.read_to_string(&mut buffer)?;
-    ctx.set_contents(buffer.trim_end().to_string())?;
+    ctx.set_contents(buffer.trim_end().to_string()).unwrap();
     // println!("{:?}", ctx.get_contents()?);
     Ok(())
 }
