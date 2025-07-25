30969378db1d313737521d550a0adb49d7bca21d
macroexpansion <macroexpansion@gmail.com>
2025-07-25 12:41:01 +0700

    v0.1.3

--- src/content_copy.rs
+++ src/content_copy.rs
@@ -1,11 +1,19 @@
 use std::{error::Error, fs::File, io::Read};
 
-use cli_clipboard::{get_contents, set_contents};
+use cli_clipboard::{ClipboardContext, ClipboardProvider};
 
 pub fn clipboard_copy(src_file_path: &str) -> Result<(), Box<dyn Error>> {
     let mut src_file = File::open(src_file_path)?;
+    let mut ctx = ClipboardContext::new()?;
+
     let mut buffer: String = String::new();
     src_file.read_to_string(&mut buffer)?;
-    set_contents(buffer.trim_end().to_string())?;
+
+    ctx.set_contents(buffer.trim_end().to_string())?;
+
+    let _resp = ctx.get_contents()?;
+    // println!("{:?}", _resp);
+    println!("Copied to clipboard");
+
     Ok(())
 }
